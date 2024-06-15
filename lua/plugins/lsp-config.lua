7ec75d97b6e2ffb04fa282231cfaf68a3e286246
Vloneskorpion <kamilnowak432@gmail.com>
2024-06-15 22:34:50 +0200

    Added some features, colorizer and changed theme

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -22,6 +22,7 @@ return {
       lspconfig.pylsp.setup({})
       lspconfig.clangd.setup({})
       lspconfig.bashls.setup({})
+      lspconfig.cmake.setup({})
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,5 +6,8 @@ vim.cmd("set number")
 
 vim.cmd("set wildmenu")
 
+vim.keymap.set('n', 'gO', "<Cmd>call append(line('.') - 1, repeat([''], v:count1))<CR>")
+vim.keymap.set('n', 'go', "<Cmd>call append(line('.'),     repeat([''], v:count1))<CR>")
+
 vim.g.mapleader = " "
 
